6d79c2ca71bf8d4d32e49e14dc7c6b5e77634b57
dididiri1 <dididiri1@naver.com>
2023-07-13 18:47:36 +0900

    commit

--- section1_spring/src/main/java/Main.java
+++ section1_spring/src/main/java/Main.java
@@ -3,44 +3,41 @@ import java.util.Scanner;
 class Main {
 
     /*
-      8. 유효한 팰린드롬
+      9. 숫자만 추출
       설명
+        문자와 숫자가 섞여있는 문자열이 주어지면 그 중 숫자만 추출하여 그 순서대로 자연수를 만듭니다.
 
-        앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 팰린드롬이라고 합니다.
+        만약 “tge0a1h205er”에서 숫자만 추출하면 0, 1, 2, 0, 5이고 이것을 자연수를 만들면 1205이 됩니다.
 
-        문자열이 입력되면 해당 문자열이 팰린드롬이면 "YES", 아니면 “NO"를 출력하는 프로그램을 작성하세요.
-
-        단 회문을 검사할 때 알파벳만 가지고 회문을 검사하며, 대소문자를 구분하지 않습니다.
-
-        알파벳 이외의 문자들의 무시합니다.
+        추출하여 만들어지는 자연수는 100,000,000을 넘지 않습니다.
 
 
       입력
-        첫 줄에 길이 100을 넘지 않는 공백이 없는 문자열이 주어집니다.
-
+        첫 줄에 숫자가 섞인 문자열이 주어집니다. 문자열의 길이는 100을 넘지 않습니다.
 
       출력
-        첫 번째 줄에 팰린드롬인지의 결과를 YES 또는 NO로 출력합니다.
+        첫 줄에 자연수를 출력합니다.
 
 
       예시 입력 1
-        found7, time: study; Yduts; emit, 7Dnuof
 
+        g0en2T0s8eSoft
       예시 출력 1
-        YES
+
+        208
      */
     public String solution(String str) {
-        String answer = "YES";
+        String answer = "";
 
-        str = str.toUpperCase().replaceAll("[^A-Z]", "");
+        str = str.toUpperCase().replaceAll("[A-Z]", "");
 
-        int len = str.length();
+        System.out.println("str = " + str);
 
-        for (int i = 0; i < len/2; i++) {
-            if(str.charAt(i) != str.charAt(len-i-1)) {
-                answer = "NO";
-            }
-        }
+        int num = Integer.valueOf(str);
+
+        answer = String.valueOf(num);
+
+
         return answer;
     }
 
