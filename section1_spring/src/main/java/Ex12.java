e5483cbb9ef012f4e3a72a4a2630f4f1a205c827
dididiri1 <dididiri1@naver.com>
2023-07-15 12:14:11 +0900

    1. 큰 수 출력하기 알고리즘 풀이 추가

--- section1_spring/src/main/java/Ex12.java
+++ section1_spring/src/main/java/Ex12.java
@@ -1,2 +1,29 @@
-public class Ex12 {
+import java.util.Scanner;
+
+class Ex12 {
+
+    public String solution(int n, String s) {
+        String answer = "";
+
+        for (int i = 0; i < n; i++) {
+            String tmp = s.substring(0, 7).replace('#', '1').replace('*', '0');
+            int num = Integer.parseInt(tmp, 2);
+            char c = (char) num;
+            answer += c;
+
+            System.out.println("tmp = " + tmp);
+            s = s.substring(7);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Ex12 T = new Ex12();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        String str = kb.next();
+
+        System.out.println(T.solution(n, str));
+    }
 }
--- section1_spring/src/main/java/Main.java
+++ section1_spring/src/main/java/Main.java
@@ -1,29 +1,17 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 class Main {
-
-    public String solution(int n, String s) {
-        String answer = "";
-
-        for (int i = 0; i < n; i++) {
-            String tmp = s.substring(0, 7).replace('#', '1').replace('*', '0');
-            int num = Integer.parseInt(tmp, 2);
-            char c = (char) num;
-            answer += c;
-
-            System.out.println("tmp = " + tmp);
-            s = s.substring(7);
-        }
-
+    public ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        z
         return answer;
     }
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
-        int n = kb.nextInt();
-        String str = kb.next();
 
-        System.out.println(T.solution(n, str));
+
     }
 }
