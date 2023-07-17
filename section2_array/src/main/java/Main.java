d0577dde87e096507a4af8087198ec750624fd26
dididiri1 <dididiri1@naver.com>
2023-07-17 14:13:28 +0900

    8. 등수구하기 알고리즘 풀이 추가

--- section2_array/src/main/java/Main.java
+++ section2_array/src/main/java/Main.java
@@ -2,17 +2,9 @@ import java.util.Scanner;
 
 class Main {
 
-    public int solution(int n, int[] arr) {
-        int answer = 0, cnt = 0;
+    public int[] solution(int n, int[] arr) {
+        int[] answer = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == 1) {
-                cnt ++;
-                answer += cnt;
-            } else {
-                cnt = 0;
-            }
-        }
 
         return answer;
     }
@@ -20,14 +12,6 @@ class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
-        int n = kb.nextInt();
-        int arr[] = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = kb.nextInt();
-        }
-
-        System.out.println(T.solution(n, arr));
 
     }
 }
