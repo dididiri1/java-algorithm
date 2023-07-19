78eab5aa6b8d0d25a96092bf036843ab6ca682d3
dididiri1 <dididiri1@naver.com>
2023-07-19 15:11:25 +0900

    11. 임시반장 정하기 알고리즘 풀이 추

--- section2_array/src/main/java/Main.java
+++ section2_array/src/main/java/Main.java
@@ -2,29 +2,10 @@ import java.util.Scanner;
 
 class Main {
 
-    int[] dx = {-1, 0, 1, 0};
-    int[] dy = {0, 1, 0, -1};
-
     public int solution(int n, int[][] arr) {
-        int answer = 0;
+        int answer = 0, max = Integer.MIN_VALUE;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                boolean flag = true;
-                for (int k = 0; k < 4; k++) {
-                    int nx = i + dx[k];
-                    int ny = j + dy[k];
-                    if (nx >= 0 && nx < n && ny >= 0 && ny < n && arr[nx][ny] >= arr[i][j]) {
-                        flag = false;
-                        break;
-                    }
-                }
-                if (flag) {
-                    answer ++;
-                }
-            }
 
-        }
 
         return answer;
     }
@@ -33,13 +14,6 @@ class Main {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
-        int[][] arr = new int[n][n];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = kb.nextInt();
-            }
-        }
-        System.out.println(T.solution(n, arr));
     }
 }
