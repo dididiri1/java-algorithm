8dd88fd041113337cfde2c4cb3dc22fc10f082de
dididiri1 <dididiri1@naver.com>
2023-07-21 12:09:42 +0900

    5. 연속된 자연수의 합 알고리즘 풀이 추가

--- section3/src/main/java/Main.java
+++ section3/src/main/java/Main.java
@@ -2,10 +2,28 @@ import java.util.Scanner;
 
 class Main {
 
-    public int solution(int n, int m, int[] arr) {
-        int answer = 0;
+    public int solution(int n) {
+        int answer = 0, sum = 0, lt = 0;
+        int m = n/2+1;
+        int[] arr = new int[m];
 
+        for (int i = 0; i < m; i++) {
+            arr[i] = i+1;
+        }
 
+        for (int rt = 0; rt < m; rt++) {
+            sum += arr[rt];
+            if (sum == n) {
+                answer ++;
+            }
+
+            while (sum >= n) {
+                sum -= arr[lt++];
+                if (sum == n) {
+                    answer ++;
+                }
+            }
+        }
 
         return answer;
     }
@@ -13,6 +31,7 @@ class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
-
+        int n = kb.nextInt();
+        System.out.println(T.solution(n));
     }
 }
