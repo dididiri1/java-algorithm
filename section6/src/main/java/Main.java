ebea259c1e4a10e8ef838bc3d19333f18aa4d013
dididiri1 <dididiri1@naver.com>
2023-08-05 13:45:14 +0900

    버블정렬 정리 추가

--- section6/src/main/java/Main.java
+++ section6/src/main/java/Main.java
@@ -2,19 +2,10 @@ import java.util.Scanner;
 
 public class Main {
 
-    public int[] solution(int n, int[] arr) {
-        for (int i = 0; i < n-1; i++) {
-            int idx = i;
-            for (int j = i+1; j < n; j++) {
-                if (arr[j] < arr[idx]) {
-                    idx = j;
-                }
-            }
-            int tmp = arr[i];
-            arr[i] = arr[idx];
-            arr[idx] = tmp;
-        }
-        return arr;
+    public int solution(int n, int[] arr) {
+        int answer = 0;
+
+        return answer;
     }
 
     public static void main(String[] args) {
@@ -22,12 +13,6 @@ public class Main {
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = kb.nextInt();
-        }
 
-        for (int x : T.solution(n, arr)) {
-            System.out.println(x+ " ");
-        }
     }
 }
