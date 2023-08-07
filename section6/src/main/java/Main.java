e19d41a60bcbc7e3d7fb46956a5b81fcaeb4a407
dididiri1 <dididiri1@naver.com>
2023-08-07 11:38:37 +0900

    4. Least Recently Used 알고리즘 풀이 추가

--- section6/src/main/java/Main.java
+++ section6/src/main/java/Main.java
@@ -1,46 +1,19 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
-    private static void bubbleSort(int[] arr) {
-        bubbleSort(arr, arr.length - 1);
-    }
+    public int solution(int n, int[] arr) {
+        int answer = 0;
 
-    private static void bubbleSort(int[] arr, int last) {
-        if (last > 0) {
-            for (int i = 1; i <= last; i++) {
-                if (arr[i - 1] > arr[i]) {
-                    swap(arr, i - 1, i);
-                }
-            }
-            bubbleSort(arr, last - 1);
-        }
-    }
-
-    private static void swap(int[] arr, int source, int target) {
-        int tmp = arr[source];
-        arr[source] = arr[target];
-        arr[target] = tmp;
-    }
-
-    private static void printArray(int[] arr) {
-        for (int x : arr) {
-            System.out.println(x + " ");
-        }
-        System.out.println();
+        return answer;
     }
 
     public static void main(String[] args) {
+        Main T = new Main();
         Scanner kb = new Scanner(System.in);
+        int size = kb.nextInt();
         int n = kb.nextInt();
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = kb.nextInt();
-        }
-
-        printArray(arr);
-        bubbleSort(arr);
-        printArray(arr);
 
     }
 }
