1bc7ac02240406423a7dd8f622af648e19ae9674
dididiri1 <dididiri1@naver.com>
2023-08-16 10:53:17 +0900

    6. 부분집합 구하기(DFS) 알고리즘 풀이 추가

---
+++ section7/src/main/java/Ex06.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Ex06 {
+    static int n;
+    static int[] ch;
+
+    public void DFS(int L) {
+        if (L == n+1) {
+            String tmp = "";
+            for (int i = 1; i <= n; i++) {
+                if (ch[i] == 1) {
+                    tmp += (i+" ");
+                }
+            }
+
+            if (tmp.length() > 0) {
+                System.out.println(tmp);
+            }
+
+        } else {
+            ch[L] = 1;
+            DFS(L+1);
+            ch[L] = 0;
+            DFS(L+1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Ex06 T = new Ex06();
+        n = 3;
+        ch = new int[n+1];
+        T.DFS(1);
+    }
+}
