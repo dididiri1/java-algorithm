04e454550e91e62ff51b5e1488fdc6bf44941894
dididiri1 <dididiri1@naver.com>
2023-08-16 16:32:29 +0900

    10. Tree 가장 짧은경로 BFS 알고리즘 풀이 추가

--- section7/src/main/java/Main.java
+++ section7/src/main/java/Main.java
@@ -1,15 +1,33 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
 
     Node root;
 
-    public int DFS(int L, Node root) {
+    public int BFS(Node root) {
+        Queue<Node> Q = new LinkedList<>();
+        Q.offer(root);
+        int L = 0;
+        while (!Q.isEmpty()) {
+            int len = Q.size();
+            for (int i = 0; i < len; i++) {
+                Node cur = Q.poll();
+                if (cur.lt == null && cur.rt == null) {
+                    return L;
+                }
 
-        if (root.lt == null && root.rt == null) {
-            return L;
-        } else {
-            return Math.min(DFS(L+1, root.lt), DFS(L+1, root.rt));
+                if (cur.lt != null) {
+                    Q.offer(cur.lt);
+                }
+
+                if (cur.rt != null){
+                    Q.offer(cur.rt);
+                }
+            }
+            L ++;
         }
+        return 0;
     }
 
     public static void main(String[] args) {
@@ -19,6 +37,6 @@ public class Main {
         tree.root.rt = new Node(3);
         tree.root.lt.lt = new Node(4);
         tree.root.lt.rt = new Node(5);
-        System.out.println(tree.DFS(0, tree.root));
+        System.out.println(tree.BFS(tree.root));
     }
 }
