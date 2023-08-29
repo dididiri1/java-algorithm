0ced3d3c99578b31b139deb849dccb272e4cf43a
dididiri1 <dididiri1@naver.com>
2023-08-29 11:20:19 +0900

    1. 씨름선수 알고리즘 풀이 추가

---
+++ section9/src/main/java/Main.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+
+    public int solution(ArrayList<Body> arr, int n) {
+        int cnt = 0;
+        Collections.sort(arr);
+        int max = Integer.MIN_VALUE;
+
+        for (Body ob : arr) {
+            if (ob.w > max) {
+                max = ob.w;
+                cnt ++;
+            }
+        }
+
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        ArrayList<Body> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int h = kb.nextInt();
+            int w = kb.nextInt();
+            arr.add(new Body(h, w));
+        }
+        System.out.println(T.solution(arr, n));
+    }
+}
