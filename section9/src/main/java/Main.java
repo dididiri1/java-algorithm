b31fdf7e506b2833631107e97f27a061afa6df32
dididiri1 <dididiri1@naver.com>
2023-08-30 10:14:11 +0900

    3. 결혼식 알고리즘 풀이 추가

--- section9/src/main/java/Main.java
+++ section9/src/main/java/Main.java
@@ -1,52 +1,23 @@
 import java.util.ArrayList;
+
 import java.util.Collections;
 import java.util.Scanner;
 
-class Time implements Comparable<Time> {
 
-    public int s, e;
-
-    Time(int s, int e) {
-        this.s = s;
-        this.e = e;
-    }
-
-    @Override
-    public int compareTo(Time o) {
-        if(this.e == o.e) {
-            return this.s-o.s;
-        } else {
-            return this.e-o.e;
-        }
-    }
-}
 
 public class Main {
 
-    public int solution(ArrayList<Time> arr, int n) {
-        int cnt = 0;
-        Collections.sort(arr);
-        int et = 0;
-        for (Time ob : arr) {
-            if (ob.s >= et) {
-                cnt ++;
-                et = ob.e;
-            }
-        }
+    public int solution(ArrayList<Time> arr) {
+        int answer = Integer.MIN_VALUE;
 
-        return cnt;
+
+        return answer;
     }
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
-        ArrayList<Time> arr = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            int e = kb.nextInt();
-            int s = kb.nextInt();
-            arr.add(new Time(e, s));
-        }
-        System.out.println(T.solution(arr, n));
+
     }
 }
