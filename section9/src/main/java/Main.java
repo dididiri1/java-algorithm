eb7e880834df54b35964e19159d562e63b7bd415
dididiri1 <dididiri1@naver.com>
2023-09-01 10:24:09 +0900

    8. 원더랜드(최소 스패닝 트리 - 프림 : PriorityQueue 활용) 알고리즘 풀이 추가

--- section9/src/main/java/Main.java
+++ section9/src/main/java/Main.java
@@ -1,13 +1,11 @@
 import java.util.*;
 
 class Edge implements Comparable<Edge> {
-    public int v1;
-    public int v2;
+    public int vex;
     public int cost;
 
-    public Edge(int v1, int v2, int cost) {
-        this.v1 = v1;
-        this.v2 = v2;
+    public Edge(int vex, int cost) {
+        this.vex = vex;
         this.cost = cost;
     }
 
@@ -19,49 +17,42 @@ class Edge implements Comparable<Edge> {
 
 public class Main {
 
-    static int[] unf;
-
-    public static int Find(int v) {
-        if (unf[v] == v) {
-            return v;
-        } else {
-            return unf[v] = Find(unf[v]);
-        }
-    }
-
-    public static void Union(int a, int b) {
-        int fa = Find(a);
-        int fb = Find(b);
-        if (fa != fb) {
-            unf[fa] = fb;
-        }
-    }
+    static int[] ch;
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
         int m = kb.nextInt();
-        unf = new int[n+1];
-        ArrayList<Edge> arr = new ArrayList<Edge>();
-        for (int i = 1; i < n; i++) {
-            unf[i] = i;
+        ArrayList<ArrayList<Edge>> graph = new ArrayList<ArrayList<Edge>>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<Edge>());
         }
+
+        ch = new int[n+1];
+
         for (int i = 0; i < m; i++) {
             int a = kb.nextInt();
             int b = kb.nextInt();
             int c = kb.nextInt();
-            arr.add(new Edge(a, b, c));
+            graph.get(a).add(new Edge(b ,c));
+            graph.get(b).add(new Edge(a ,c));
         }
         int answer = 0;
-        Collections.sort(arr);
+        PriorityQueue<Edge> pQ = new PriorityQueue<>();
+        pQ.offer(new Edge(1, 0));
 
-        for (Edge ob : arr) {
-            int fv1 = Find(ob.v1);
-            int fv2 = Find(ob.v2);
-            if (fv1 != fv2) {
-                answer += ob.cost;
-                Union(ob.v1, ob.v2);
+        while (!pQ.isEmpty()) {
+            Edge tmp = pQ.poll();
+            int ev = tmp.vex;
+            if (ch[ev] == 0) {
+                ch[ev] = 1;
+                answer += tmp.cost;
+                for (Edge ob : graph.get(ev)) {
+                    if (ch[ob.vex] == 0) {
+                        pQ.offer(new Edge(ob.vex, ob.cost));
+                    }
+                }
             }
         }
 
