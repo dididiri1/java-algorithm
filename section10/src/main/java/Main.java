5cec62f65a90a570fed19886fb63684d6cc1f440
dididiri1 <dididiri1@naver.com>
2023-09-05 12:20:39 +0900

    6. 최대점수 구하기(냅색 알고리즘) 알고리즘 풀이 추가

--- section10/src/main/java/Main.java
+++ section10/src/main/java/Main.java
@@ -1,34 +1,21 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
-    static int n, m;
-
-    static int[] dy;
-
-    public int solution(int[] coin) {
-        Arrays.fill(dy, Integer.MAX_VALUE);
-        dy[0] = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = coin[i]; j <= m; j++) {
-                dy[j] = Math.min(dy[j], dy[j- coin[i]] +1);
-            }
-        }
-
-        return dy[m];
-    }
-
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
-        n = kb.nextInt();
-        int[] arr = new int[n];
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+        int[] dy = new int[m+1];
+
         for (int i = 0; i < n; i++) {
-            arr[i] = kb.nextInt();
+            int ps = kb.nextInt();
+            int pt = kb.nextInt();
+            for (int j = m; j >= pt; j--) {
+                dy[j] = Math.max(dy[j], dy[j - pt] + ps);
+            }
         }
-        m = kb.nextInt();
-        dy = new int[m+1];
-        System.out.println(T.solution(arr));
+        System.out.println(dy[m]);
     }
 }
