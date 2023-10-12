2bfd79580fb0fcda5cbecc803accdbeab8e4ed30
dididiri1 <dididiri1@naver.com>
2023-10-12 11:32:10 +0900

    [복습] 3. 문장속 단어 알고리즘 추가

--- secction11/src/main/java/Main.java
+++ secction11/src/main/java/Main.java
@@ -4,13 +4,14 @@ public class Main {
 
     public String solution(String str) {
         String answer = "";
+        String[] strArray = str.split(" ");
+        int m = Integer.MIN_VALUE;
 
-        for (int i=0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            if (Character.isLowerCase(c)) {
-                answer += Character.toUpperCase(c);
-            } else {
-                answer += Character.toLowerCase(c);
+        for(String s : strArray) {
+            int len = s.length();
+            if (m < len) {
+                m = len;
+                answer = s;
             }
         }
 
@@ -21,7 +22,7 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
-        String str = kb.next();
+        String str = kb.nextLine();
         System.out.println(T.solution(str));
     }
 }
