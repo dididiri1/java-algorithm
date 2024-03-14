62d8f2e8b4405e1a5a6f807d411f98258d6f39ca
dididiri1 <dididiri1@naver.com>
2024-03-14 16:29:37 +0900

    commit

--- secction11/src/main/java/Main.java
+++ secction11/src/main/java/Main.java
@@ -6,23 +6,18 @@ public class Main {
 
     public int solution(int n) {
         int answer = 0;
+        int[] ch = new int[n+1];
+
         for(int i = 2; i < n; i++) {
-            if (isPrimeNumber(i)) {
+            if (ch[i] == 0) {
                 answer ++;
+                for (int j = i; j < n; j = j + i) {
+                    ch[j] = 1;
+                }
             }
         }
 
         return answer;
-    }
-
-    public boolean isPrimeNumber(int x) {
-        for (int i = 2; i < x; i++) {
-            if (x % i == 0) {
-                return false;
-            }
-        }
-
-        return true;
     }
 
     public static void main(String[] args) {
